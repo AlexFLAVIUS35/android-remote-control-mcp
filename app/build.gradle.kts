3ffe40d30afb7bc45d0d6e1a0bc5b7c570d8b026
Clyferf <114229198+AlexFLAVIUS35@users.noreply.github.com>
2026-09-22 17:20:45 +0300

    Enable Ktor WebSockets for screen stream

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -358,6 +358,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.cors)
+    implementation(libs.ktor.server.websockets)
     implementation(libs.ktor.network.tls.certificates)
     implementation(libs.ktor.serialization.kotlinx.json)
 
