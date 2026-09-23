153bcfce6303929a956633fe14af55584af65f78
Clyferf <114229198+AlexFLAVIUS35@users.noreply.github.com>
2026-09-23 14:02:26 +0300

    fix: satisfy ktlint when branch braces

--- app/src/main/kotlin/com/danielealbano/androidremotecontrolmcp/services/screencapture/ScreenStreamService.kt
+++ app/src/main/kotlin/com/danielealbano/androidremotecontrolmcp/services/screencapture/ScreenStreamService.kt
@@ -260,7 +260,9 @@ class ScreenStreamService : Service() {
                 ScreenStreamHub.FLAG_KEY_FRAME
             }
 
-            else -> 0
+            else -> {
+                0
+            }
         }
 
     private fun requestSyncFrame() {
